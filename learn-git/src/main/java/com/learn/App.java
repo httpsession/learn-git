fecd456e29fc3ba6a6903a8d32421b0e4e36cefa
lilongjie <lilongjie@gomo.com>
2018-12-18 19:12:31 +0800

    what can I do for you

--- learn-git/src/main/java/com/learn/App.java
+++ learn-git/src/main/java/com/learn/App.java
@@ -4,5 +4,8 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println();
+		System.out.println("nothing to do");
+		System.out.println("something to do ");
+		System.out.println("what can I do for you !");
 	}
 }
