ca70374f8f097e50b8a035d2bb9489c8c05cfd73
lilongjie <lilongjie@gomo.com>
2018-12-18 17:14:05 +0800

    something to do

--- learn-git/src/main/java/com/learn/App.java
+++ learn-git/src/main/java/com/learn/App.java
@@ -4,5 +4,7 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println();
+		System.out.println("nothing to do");
+		System.out.println("something to do ");
 	}
 }
