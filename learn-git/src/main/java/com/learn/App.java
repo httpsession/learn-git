bc2884b32ab40c330d8912c6b68b2af15cf82c26
lilongjie <lilongjie@gomo.com>
2018-12-19 09:36:30 +0800

    test9

--- learn-git/src/main/java/com/learn/App.java
+++ learn-git/src/main/java/com/learn/App.java
@@ -13,9 +13,5 @@ public class App {
 		System.out.println("test7!");
 		System.out.println("test8");
 		System.out.println("test9");
-		System.out.println("test10");
-		System.out.println("test11");
-		System.out.println("test12");
-		System.out.println("test13");
 	}
 }
