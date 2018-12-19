c2c577355eeeed691d317e78876b533d6a6abf84
lilongjie <lilongjie@gomo.com>
2018-12-19 09:37:03 +0800

    test8

--- learn-git/src/main/java/com/learn/App.java
+++ learn-git/src/main/java/com/learn/App.java
@@ -2,16 +2,8 @@ package com.learn;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println("hello world");
-		System.out.println();
-		System.out.println("nothing to do");
-		System.out.println("something to do ");
-		System.out.println("hfhsdafsdaf");
-		System.out.println();
-		System.out.println("上班！");
 		System.out.println("test6!");
 		System.out.println("test7!");
 		System.out.println("test8");
-		System.out.println("test9");
 	}
 }
